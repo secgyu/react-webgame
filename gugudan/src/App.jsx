586fdc65dfc1527123d8d7e270425aa961f27634
secgyu <abc2232002@gmail.com>
2025-01-07 23:57:05 +0900

    first commit

---
+++ gugudan/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const answer = first * second;
+    if (parseInt(value) === answer) {
+      setResult("정답!");
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue("");
+    } else {
+      setResult("땡!");
+      setValue("");
+    }
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        {first}곱하기{second}는?
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={onChange} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+}
+
+export default App;
